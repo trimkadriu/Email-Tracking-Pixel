97f3d7018949379af6e47940a516e87e935a569c
Trim Kadriu <trim.kadriu@gmail.com>
2015-03-23 14:38:40 +0100

    Adding improvements for easier usage

--- check.php
+++ check.php
@@ -1,14 +1,25 @@
 <h3>Track your email</h3>
-<label>Generate TRACKING ID by writing below:</label>
-<input type="text" id="imgidGenerator"/>
-<input type="submit" value="Generate" onclick="generateImgId()"/><br/><br/>
-<div id="generatedImgid"></div>
+<form method="GET" id="generateForm">
+   <label>Generate TRACKING ID by writing below:</label><br/>
+   <input type="text" name="generateid" id="imgidGenerator"/>
+   <input type="button" value="Generate" onclick="generateImgId()"/><br/>
+   <small>Click on generate again after you sent the email just to make sure there are no pre-fetches from the mail service</small><br/>
+<?php 
+   $genId = $_GET["generateid"];
+   $actual_link = 'http://'.$_SERVER['HTTP_HOST'].dirname($_SERVER['PHP_SELF']).'/track.php?imgid='.$genId;
+   if(isset($genId)) echo '<div id="generatedImgid"><b>'.$actual_link.'</b></div>';
+   $str=file_get_contents('./emails.db');
+   $str=str_replace($genId, "", $str);
+   file_put_contents('./emails.db', $str);
+?>
+</form>
 <script>
 function generateImgId() {
    var imgid = document.getElementById('imgidGenerator').value;
    var url = location.href.substring(0, location.href.lastIndexOf("/")+1);
    if(imgid !== '') {
-      document.getElementById('generatedImgid').innerHTML = url + 'track.php?imgid=' + imgid;
+      //document.getElementById('generatedImgid').innerHTML = url + 'track.php?imgid=' + imgid;
+      document.getElementById('generateForm').submit();
    }
    else {
       alert('ID cannot be empty');
@@ -17,11 +28,12 @@ function generateImgId() {
 </script>
 <br/><br/>
 <form method="GET">
-   <label>Track an email by some existing TRACKING ID:</label>
+   <label>Track an email by some existing TRACKING ID:</label><br/>
    <input type="text" name="imgid"/>
    <input type="submit" value="Check!"/>
 </form>
 <?php
+
 if(empty($_GET['imgid'])) exit;
 if( exec('grep '.escapeshellarg($_GET['imgid']).' ./emails.db')) {
    echo '<b>Your email is seen :)</b>';
@@ -29,3 +41,4 @@ if( exec('grep '.escapeshellarg($_GET['imgid']).' ./emails.db')) {
 else {
    echo '<b>Your email is NOT seen :(</b>';
 }
+?>
